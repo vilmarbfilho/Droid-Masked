20af2f2d11c31178bcd6cb8a3c64a276f42cdbfd
Vilmar <vilmarbf.ads@gmail.com>
2015-05-22 22:26:16 -0300

    DEV added Robolectric in the project

---
+++ droidmasked/app/src/test/java/com/vlabs/droidmasked/views/EditTextMaskedTest.java
@@ -0,0 +1,23 @@
+package com.vlabs.droidmasked.views;
+
+import com.vlabs.droidmasked.test.MainActivity;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by vilmar on 5/22/15.
+ */
+
+@RunWith(RobolectricTestRunner.class)
+@Config(manifest = "src/main/AndroidManifest.xml", emulateSdk = 22)
+public class EditTextMaskedTest {
+
+
+}
