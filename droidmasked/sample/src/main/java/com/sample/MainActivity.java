7cc445180a581b1c7caf289c8604606bcea17a49
Vilmar Bispo Filho <vilmarbf.ads@gmail.com>
2015-08-20 22:36:37 -0300

    Added implementation method addMask using array masks

--- droidmasked/sample/src/main/java/com/sample/MainActivity.java
+++ droidmasked/sample/src/main/java/com/sample/MainActivity.java
@@ -2,8 +2,6 @@ package com.sample;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.EditText;
-
 import com.vlabs.droidmasked.views.EditTextMasked;
 
 
@@ -12,15 +10,17 @@ import com.vlabs.droidmasked.views.EditTextMasked;
  */
 public class MainActivity extends AppCompatActivity {
 
-    private EditTextMasked mEditText;
+    private EditTextMasked mEditTextMasked;
+    private String[] masks = {"###-###", "###.###-##", "##-##"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mEditText = (EditTextMasked) findViewById(R.id.et_mask);
-        mEditText.addMask("{#}");
+        mEditTextMasked = (EditTextMasked) findViewById(R.id.et_mask);
+        //mEditText.addMask("###.###.###-##");
+        mEditTextMasked.addMask(masks);
     }
 
 }
