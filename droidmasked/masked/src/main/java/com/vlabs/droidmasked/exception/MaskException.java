2a645e81807265d6565e166d10216d960d8f15a9
Vilmar Bispo Filho <vilmarbf.ads@gmail.com>
2015-08-25 22:52:27 -0300

    Finish Mask Exception

--- droidmasked/masked/src/main/java/com/vlabs/droidmasked/exception/MaskException.java
+++ droidmasked/masked/src/main/java/com/vlabs/droidmasked/exception/MaskException.java
@@ -3,5 +3,9 @@ package com.vlabs.droidmasked.exception;
 /**
  * Created by vilmar.filho on 8/21/15.
  */
-public class MaskException extends Exception{
+public class MaskException extends RuntimeException{
+
+    public MaskException (String message) {
+        super(message);
+    }
 }
